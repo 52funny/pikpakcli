6c476e9d5aac8743f2340beb4909f10adfa4e32c
52funny <wwq9977@gmail.com>
2023-11-13 14:28:52 +0800

    fix: use unsafe library to transmute string to byte slice

--- internal/utils/str.go
+++ internal/utils/str.go
@@ -1,17 +1,11 @@
 package utils
 
 import (
-	"reflect"
 	"unsafe"
 )
 
-func StringToByteSlice(str string) (bs []byte) {
-	strH := (*reflect.StringHeader)(unsafe.Pointer(&str))
-	bsH := (*reflect.SliceHeader)(unsafe.Pointer(&bs))
-	bsH.Data = strH.Data
-	bsH.Len = strH.Len
-	bsH.Cap = strH.Len
-	return
+func StringToByteSlice(str string) []byte {
+	return unsafe.Slice(unsafe.StringData(str), len(str))
 }
 
 func ByteSliceToString(bs []byte) string {
